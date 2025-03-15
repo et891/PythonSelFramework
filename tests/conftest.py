5f82d451f6178f11195d8567c550c208ece0d476
artteri <mikhail.kazakov.qa@gmail.com>
2025-03-16 00:38:50 +0500

    Коммит из main

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -16,6 +17,10 @@ def setup(request):
     global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # ✅ Без UI (важно для CI/CD)
+        chrome_options.add_argument("--no-sandbox")  # ✅ Обход ограничений в CI
+        chrome_options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome()
     elif browser_name == "firefox":
         driver = webdriver.Firefox()
