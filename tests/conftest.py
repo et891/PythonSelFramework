ae9f4245d4d4626aec10aa335ab109f923f7cee2
artteri <mikhail.kazakov.qa@gmail.com>
2025-03-16 00:40:46 +0500

    Коммит из main

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,12 @@ def setup(request):
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # ✅ Без UI (важно для CI/CD)
-        chrome_options.add_argument("--no-sandbox")  # ✅ Обход ограничений в CI
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome()
+        chrome_options.add_argument("--headless")  # ✅ Запуск без UI
+        chrome_options.add_argument("--no-sandbox")  # ✅ Запуск в CI/CD
+        chrome_options.add_argument("--disable-dev-shm-usage")  # ✅ Фикс ошибок памяти
+        chrome_options.add_argument("--disable-gpu")  # ✅ Убираем GPU (ненужен в headless)
+        chrome_options.add_argument("--remote-debugging-port=9222")  # ✅ Избегаем конфликта с другим процессом Chrome
+        driver = webdriver.Chrome(options=chrome_options)
     elif browser_name == "firefox":
         driver = webdriver.Firefox()
     elif browser_name == "safari":
